1608ce7243226c0651d9c0283e664f83b657b8f3
Heorhii Ponomarenko <ge.ponomarenko@gmail.com>
2026-01-31 19:54:53 +0100

    Added authors list and showing posts by author

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+    
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=600)
     content = models.TextField()
     published_date = models.DateTimeField()
+    author = models.ForeignKey(Author, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.title
+
+    def published_recently(self):
+            return self.publish_date >= timezone.now() - timezone.delta(days = 7)
--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ import blog.views as blog_views
 
 urlpatterns = [
     path("", blog_views.post_list, name="posts_list"),
+    path("<int:post_id>/", blog_views.get_post_by_id, name="post_details"),
+    path("authors/", blog_views.author_list, name="authors_list"),
+    path("authors/<int:author_id>/", blog_views.get_post_by_author_id, name="authors_posts"),
 ]
